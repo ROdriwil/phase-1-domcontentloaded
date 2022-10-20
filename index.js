668d42c3bad963122a1c3086d791bc169eb0b7ed
william Rodriguez-Ledesma <popolol.wr.wr@gmail.com>
2022-10-19 17:55:46 -0700

    commit tree 920b3ea3d1a36bd69808c19ca726ab5b671d14f0

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    domContentUpdated();
+  });
+
+  function domContentUpdated() {
+   const javaText = document.getElementById('text');
+   javaText.textContent = 'This is really cool!';
+  }
